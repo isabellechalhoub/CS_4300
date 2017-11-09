78377368e17ecacdee68556e921787609c918830
isabellechalhoub <isabelle.chalhoub@gmail.com>
2017-11-08 21:03:45 -0700

    fixed driver

--- A5/CS4300_A5_driver.m
+++ A5/CS4300_A5_driver.m
@@ -1,22 +1,26 @@
-function [mean] = CS4300_A5_driver(num_trials)
+function [res] = CS4300_A5_driver(num_trials)
 
 load('A5_boards.mat');
 
-total_scores = zeros(1,250);
+res.total_scores = zeros(1,250);
+res.successes = zeros(1,250);
 
 for i=1:num_trials
     for j=1:250
         clear('CS4300_MC_agent');
         [scr,trc] = CS4300_WW1(50,'CS4300_MC_agent',boards(j).board);
-        total_scores(j) = total_scores(j) + scr;
+        res.total_scores(j) = res.total_scores(j) + scr;
+        if scr > 0
+            res.successes(j) = res.successes(j) + 1;
+        end
     end
 end
 
-averages = total_scores/num_trials;
-mean = 0;
+res.averages = total_scores/num_trials;
+res.mean = 0;
 for k=1:num_boards
-    mean = mean + averages(k);
+    res.mean = res.mean + res.averages(k);
 end
-mean = mean/num_boards;
+res.mean = mean/num_boards;
 end
 
